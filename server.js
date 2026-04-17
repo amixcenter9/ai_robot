31bcd9595db54e8e12af7de8472b50676a3581c9
amixcenter9 <34692848+amixcenter9@users.noreply.github.com>
2026-04-17 08:51:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
-
+const GEMINI_KEY = "ISI_API_GEMINI_KAMU";
+const OPENAI_KEY = "ISI_API_OPENAI_KAMU";
 const GEMINI_KEY = process.env.GEMINI_KEY;
 const OPENAI_KEY = process.env.OPENAI_KEY;
